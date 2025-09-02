85e9688f54ef1f9947a2599299482f0891bb47fd
Gustavo Gonçalves <gusssshit@MacBook-Air-de-Gustavo.local>
2025-09-02 17:58:22 -0300

    fix: ignore node_modules, use better-sqlite3 12.x and Node 20

--- dist/entities/habit.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from './user.entity';
-export declare class Habit {
-    id: string;
-    name: string;
-    completed: string[];
-    created: number;
-    user: User;
-}
--- dist/entities/user.entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { Habit } from './habit.entity';
-export declare class User {
-    id: string;
-    created: number;
-    habits: Habit[];
-}
